f53e9a4db7b57cd957ce57c7d91a65c0c9b45282
innocentrathogwa <innocentrathogwa@gmail.com>
2016-07-02 09:32:56 +0200

    Another list

--- 02July2016/python_Collections.py
+++ 02July2016/python_Collections.py
@@ -1,6 +1,15 @@
 mylist=['Jessy','Brain','David','Vuyisile','Mandla','Suzan','Lesley']
 print(mylist)
+
 mylist.append('Maxwell')
 print (mylist)
+
 del mylist[2]
 print(mylist)
+
+mylist.append(1)
+mylist[3]=100
+print(mylist)
+
+for i in mylist:
+    print('Hello '+str(i))
