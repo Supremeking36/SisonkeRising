3d07e3d53b5320fce2622df21eb3fda63df21c46
innocentrathogwa <innocentrathogwa@gmail.com>
2016-07-02 09:46:15 +0200

    another list

--- 02July2016/python_Collections.py
+++ 02July2016/python_Collections.py
@@ -1,15 +1,23 @@
 mylist=['Jessy','Brain','David','Vuyisile','Mandla','Suzan','Lesley']
-print(mylist)
+#print(mylist)
 
-mylist.append('Maxwell')
-print (mylist)
+#mylist.append('Maxwell')
+#print (mylist)
 
-del mylist[2]
-print(mylist)
+#del mylist[2]
+#print(mylist)
 
-mylist.append(1)
-mylist[3]=100
-print(mylist)
+#mylist.append(1)
+#mylist[3]=100
+#print(mylist)
 
 for i in mylist:
-    print('Hello '+str(i))
+    print('Hello my name is '+str(i))
+
+surlist= ['Sibitane','Kutu','Khoza','Masu','Mola','Seha']
+print(mylist + surlist)
+
+x=0
+for n in range(len(mylist)-1):
+    print('Hello I am '+ mylist[x]+' '+surlist[x] )
+    x+=1
