5490358f916725029c9773e3db0ed442292c08ca
innocentrathogwa <innocentrathogwa@gmail.com>
2016-07-02 10:43:40 +0200

    another cool list manipulation program

--- 02July2016/python_Collections.py
+++ 02July2016/python_Collections.py
@@ -1,4 +1,4 @@
-mylist=['Jessy','Brain','David','Vuyisile','Mandla','Suzan','Lesley']
+mylist=['Jessy','Brain','David','Vuyisile','Suzan','Lesley']
 #print(mylist)
 
 #mylist.append('Maxwell')
@@ -11,13 +11,17 @@ mylist=['Jessy','Brain','David','Vuyisile','Mandla','Suzan','Lesley']
 #mylist[3]=100
 #print(mylist)
 
-for i in mylist:
-    print('Hello my name is '+str(i))
+#for i in mylist:
+#    print('Hello my name is '+str(i))
 
-surlist= ['Sibitane','Kutu','Khoza','Masu','Mola','Seha']
-print(mylist + surlist)
+surlist= ['Sibitane','Kutumela','Khoza','Masuku','Mola','Sehata']
+#print(mylist + surlist)
+agelist=[12,32,4,13,45,22,21]
 
-x=0
-for n in range(len(mylist)-1):
-    print('Hello I am '+ mylist[x]+' '+surlist[x] )
-    x+=1
+collist=['Blue','Green','Red','Pink','White','Black']
+
+
+for a,b,c,d in zip(mylist,surlist,agelist,collist):
+    print('Hello I am {0} {1}, im {2} years old and my favourate colour is {3}'.format(a,b,c,d)
+          )
+
